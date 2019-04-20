cd6b898e20a418bab9f4d1c8177e5b09275a9bb8
Nikita <ndrozdov9@gmail.com>
2019-04-20 21:39:53 +0500

    'Погодочка'

--- src/index.js
+++ src/index.js
@@ -1,46 +1,62 @@
 import "babel-polyfill";
 import Chart from "chart.js";
 
-const currencyURL = "www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
-// const meteoURL = "/xml.meteoservice.ru/export/gismeteo/point/140.xml";
+// const currencyURL = "www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
+const meteoURL = "/xml.meteoservice.ru/export/gismeteo/point/140.xml";
 
 async function loadCurrency() {
-  const response = await fetch(currencyURL);
+  // const response = await fetch(currencyURL);//???????????????????????
+  const response = await fetch(meteoURL);
   const xmlTest = await response.text();
   const parser = new DOMParser();
-  const currencyData = parser.parseFromString(xmlTest, "text/xml");
-  // <Cube currency="USD" rate="1.1321" />
-  const rates = currencyData.querySelectorAll("Cube[currency][rate]");
+  // const currencyData = parser.parseFromString(xmlTest, "text/xml");
+  // // <Cube currency="USD" rate="1.1321" />
+  // const rates = currencyData.querySelectorAll("Cube[currency][rate]");
+
+  const temperatureData = parser.parseFromString(xmlTest, "text/xml"); 
+  // <TEMPERATURE max="10" min="2"/>
+  const rates = temperatureData.querySelectorAll("TEMPERATURE[max][min]");
+  const dateData = parser.parseFromString(xmlTest, "text/xml"); 
+  // <FORECAST day="20" month="04" year="2019" hour="21" tod="3" predict="0" weekday="7">
+  const dates = dateData.querySelectorAll("FORECAST[day][month][year][hour][tod][predict][weekday]");
+
   const result = Object.create(null);
   for (let i = 0; i < rates.length; i++) {
+    // const rateTag = rates.item(i);
+    // const rate = rateTag.getAttribute("rate");
+    // const currency = rateTag.getAttribute("currency");
+    // result[currency] = rate;
+
     const rateTag = rates.item(i);
-    const rate = rateTag.getAttribute("rate");
-    const currency = rateTag.getAttribute("currency");
-    result[currency] = rate;
+    const rate = rateTag.getAttribute("max");
+    const dateTag = dates.item(i);
+    const date = dateTag.getAttribute("hour");
+    result[date] = rate;
   }
-  result["EUR"] = 1;
+  // result["EUR"] = 1;
   // result["RANDOM"] = 1 + Math.random();
   return result;
 }
 
-function normalizeDataByCurrency(data, currency) {
-  const result = Object.create(null);
-  const value = data[currency];
-  for (const key of Object.keys(data)) {
-    result[key] = value / data[key];
-  }
-  return result;
-}
+
+// function normalizeDataByCurrency(data, currency) {
+//   const result = Object.create(null);
+//   const value = data[currency];
+//   for (const key of Object.keys(data)) {
+//     result[key] = value / data[key];
+//   }
+//   return result;
+// }
 
 const buttonBuild = document.getElementById("btn");
 const canvasCtx = document.getElementById("out").getContext("2d");
 buttonBuild.addEventListener("click", async function() {
   const currencyData = await loadCurrency();
-  const normalData = normalizeDataByCurrency(currencyData, "RUB");
-  const keys = Object.keys(normalData).sort((k1, k2) =>
-    compare(normalData[k1], normalData[k2])
+  // const normalData = normalizeDataByCurrency(currencyData, "RUB");
+  const keys = Object.keys(currencyData).sort((k1, k2) =>
+    compare(currencyData[k1], currencyData[k2])
   );
-  const plotData = keys.map(key => normalData[key]);
+  const plotData = keys.map(key => currencyData[key]);
 
   const chartConfig = {
     type: "line",
@@ -49,7 +65,7 @@ buttonBuild.addEventListener("click", async function() {
       labels: keys,
       datasets: [
         {
-          label: "Стоимость валюты в рублях",
+          label: "Температура",
           backgroundColor: "rgb(255, 20, 20)",
           borderColor: "rgb(180, 0, 0)",
           data: plotData
@@ -68,6 +84,7 @@ buttonBuild.addEventListener("click", async function() {
   } else {
     window.chart = new Chart(canvasCtx, chartConfig);
   }
+  
 });
 
 function compare(a, b) {
@@ -75,3 +92,7 @@ function compare(a, b) {
   if (a < b) return -1;
   return 0;
 }
+
+
+//RGB(20, 130, 50)
+//RGB(10, 200, 50)
